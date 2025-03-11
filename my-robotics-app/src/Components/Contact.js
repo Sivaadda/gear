7cbb740eadcbf2e3c4bfdea760d9c07d85143d5e
sivaadda <sivapriyaadda@gmail.com>
2025-03-12 03:42:21 +0530

    removed unnessary code

--- my-robotics-app/src/Components/Contact.js
+++ my-robotics-app/src/Components/Contact.js
@@ -4,42 +4,7 @@ import './Contact.css';
 
 function Contact() {
   return (
-    <div className="animated-home-page">
-      <header className="animated-header">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container">
-            <Link className="navbar-brand" to="/">My Website</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav ms-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/">Home</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/about">About Us</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/services">Services</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/product">Products</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/contact">Contact Us</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header>
-      <section className="animated-intro">
-        <h1 className="animated-text">Welcome to My Creative Space</h1>
-        <p className="animated-subtext">Innovating the world with automation and technology.</p>
-        <button className="animated-button">Explore More</button>
-      </section>
-    </div>
+    <div>contus page</div>
   );
 }
 
