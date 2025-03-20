76d14b59c467abbd25d6cc71343b720b40525ff8
Sivaadda <87114326+Sivaadda@users.noreply.github.com>
2025-03-20 15:46:31 +0530

    Update App.js

--- my-robotics-app/src/App.js
+++ my-robotics-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
     <Router>
     <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/gear" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/services" element={<Services />} />
         <Route path="/products" element={<Product />} />
