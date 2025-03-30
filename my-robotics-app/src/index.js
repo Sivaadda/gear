6ce262c25b5119c7f902ed6b1cacd39d2d9bf63c
Sivaadda <87114326+Sivaadda@users.noreply.github.com>
2025-03-30 15:59:54 +0530

    Update index.js

--- my-robotics-app/src/index.js
+++ my-robotics-app/src/index.js
@@ -10,7 +10,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/"> {/* Add basename here if needed */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
